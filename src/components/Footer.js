0641a49af6634c71871b83fdd9c70a8d546f1c91
ALFAREZ SYAHPUTRA KURI <kurialfarez@gmail.com>
2025-03-02 09:57:55 +0700

    react-web-porto

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../assets/css/style.css';
+import '../assets/css/mediaqueries.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; 2023 Alfarez Syahputra Kuri. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Profile.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../assets/css/style.css';
+import '../assets/css/mediaqueries.css';
+
+const Profile = () => {
+  return (
+    <section id="profile">
+      <div className="section__pic-container">
+      <img src="./assets/img/profile-pic-2.JPG" alt="Alfarez Syahputra Kuri profile picture" />
+      </div>
+      <div className="section__text">
+        <p className="section__text__p1">Hello, I'm</p>
+        <h1 className="title">Alfarez Syahputra Kuri</h1>
+        <p className="section__text__p2">Frontend Developer</p>
+        <div className="btn-container">
+          <button
+            className="btn btn-color-2"
+            onClick={() => window.open('./assets/porto/resume-example.pdf')}
+          >
+            Download CV
+          </button>
+          <button className="btn btn-color-1" onClick={() => window.location.href='./#contact'}>
+            Contact Info
+          </button>
+        </div>
+        <div id="socials-container">
+          <img
+            src="./assets/img/linkedin.png"
+            alt="My LinkedIn profile"
+            className="icon"
+            onClick={() => window.location.href='https://linkedin.com/'}
+          />
+          <img
+            src="./assets/img/github.png"
+            alt="My Github profile"
+            className="icon"
+            onClick={() => window.location.href='https://github.com/'}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
