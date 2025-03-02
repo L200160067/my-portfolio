09516ca5045a4911b93daca1c7d6a3ba0f52ae70
ALFAREZ SYAHPUTRA KURI <kurialfarez@gmail.com>
2025-03-02 13:58:14 +0700

    new

--- src/components/About.js
+++ src/components/About.js
@@ -1,68 +1,49 @@
 import React from 'react';
-import '../assets/css/style.css';
-import '../assets/css/mediaqueries.css';
 import profilePic from '../assets/img/profile-pic.JPG';
 import experienceIcon from '../assets/img/experience.png';
 import educationIcon from '../assets/img/education.png';
 import arrowIcon from '../assets/img/arrow.png';
 
 const About = () => {
-  const aboutData = {
-    profilePic: profilePic,
-    experience: {
-      icon: experienceIcon,
-      title: 'Experience',
-      description: '2+ years\nFrontend Development',
-    },
-    education: {
-      icon: educationIcon,
-      title: 'Education',
-      description: 'B.Sc. Bachelors Degree\nM.Sc. Masters Degree',
-    },
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis reprehenderit et laborum, rem, dolore eum quod voluptate exercitationem nobis, nihil esse debitis maxime facere minus sint delectus velit in eos quo officiis explicabo deleniti dignissimos. Eligendi illum libero dolorum cum laboriosam corrupti quidem, reiciendis ea magnam? Nulla, impedit fuga!',
-  };
-
-  const details = [
-    {
-      id: 1,
-      icon: aboutData.experience.icon,
-      title: aboutData.experience.title,
-      description: aboutData.experience.description,
-    },
-    {
-      id: 2,
-      icon: aboutData.education.icon,
-      title: aboutData.education.title,
-      description: aboutData.education.description,
-    },
-  ];
-
   return (
     <section id="about">
       <p className="section__text__p1">Get To Know More</p>
       <h1 className="title">About Me</h1>
       <div className="section-container">
         <div className="section__pic-container">
-          <img src={aboutData.profilePic} alt="Profile picture" className="about-pic" />
+          <img src={profilePic} alt="Profile" className="about-pic" />
         </div>
         <div className="about-details-container">
           <div className="about-containers">
-            {details.map((detail) => (
-              <div key={detail.id} className="details-container">
-                <img src={detail.icon} alt={detail.title} className="icon" />
-                <h3>{detail.title}</h3>
-                <p>{detail.description}</p>
-              </div>
-            ))}
+            <div className="details-container">
+              <img src={experienceIcon} alt="Experience" className="icon" />
+              <h3>Experience</h3>
+              <p>2+ years <br />Frontend Development</p>
+            </div>
+            <div className="details-container">
+              <img src={educationIcon} alt="Education" className="icon" />
+              <h3>Education</h3>
+              <p>B.Sc. Bachelors Degree<br />M.Sc. Masters Degree</p>
+            </div>
           </div>
           <div className="text-container">
-            <p>{aboutData.bio}</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
+              reprehenderit et laborum, rem, dolore eum quod voluptate
+              exercitationem nobis, nihil esse debitis maxime facere minus sint
+              delectus velit in eos quo officiis explicabo deleniti dignissimos.
+              Eligendi illum libero dolorum cum laboriosam corrupti quidem,
+              reiciendis ea magnam? Nulla, impedit fuga!
+            </p>
           </div>
         </div>
       </div>
-      <a href="#experience">
-        <img src={arrowIcon} alt="Arrow icon" className="icon arrow" />
-      </a>
+      <img
+        src={arrowIcon}
+        alt="Arrow"
+        className="icon arrow"
+        onClick={() => window.location.href='./#experience'}
+      />
     </section>
   );
 };
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,61 +1,43 @@
 import React from 'react';
-import '../assets/css/style.css';
-import '../assets/css/mediaqueries.css';
 import profilePic from '../assets/img/profile-pic-2.JPG';
 import linkedinIcon from '../assets/img/linkedin.png';
 import githubIcon from '../assets/img/github.png';
 import resumePdf from '../assets/porto/resume-example.pdf';
 
 const Profile = () => {
-  const profileData = {
-    name: 'Alfarez Syahputra Kuri',
-    role: 'Frontend Developer',
-    resumeLink: resumePdf,
-    socialLinks: [
-      {
-        icon: linkedinIcon,
-        alt: 'My LinkedIn profile',
-        url: 'https://linkedin.com/',
-      },
-      {
-        icon: githubIcon,
-        alt: 'My Github profile',
-        url: 'https://github.com/',
-      },
-    ],
-  };
-
   return (
     <section id="profile">
       <div className="section__pic-container">
-        <img src={profilePic} alt={`${profileData.name} profile picture`} />
+        <img src={profilePic} alt="Profile" />
       </div>
       <div className="section__text">
         <p className="section__text__p1">Hello, I'm</p>
-        <h1 className="title">{profileData.name}</h1>
-        <p className="section__text__p2">{profileData.role}</p>
+        <h1 className="title">Alfarez Syahputra Kuri</h1>
+        <p className="section__text__p2">Frontend Developer</p>
         <div className="btn-container">
           <button
             className="btn btn-color-2"
-            onClick={() => window.open(profileData.resumeLink)}
+            onClick={() => window.open(resumePdf)}
           >
             Download CV
           </button>
-          <a href="#contact">
-            <button className="btn btn-color-1">Contact Info</button>
-          </a>
+          <button className="btn btn-color-1" onClick={() => window.location.href='./#contact'}>
+            Contact Info
+          </button>
         </div>
         <div id="socials-container">
-          {profileData.socialLinks.map((social, index) => (
-            <a
-              key={index}
-              href={social.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={social.icon} alt={social.alt} className="icon" />
-            </a>
-          ))}
+          <img
+            src={linkedinIcon}
+            alt="LinkedIn"
+            className="icon"
+            onClick={() => window.location.href='https://linkedin.com/'}
+          />
+          <img
+            src={githubIcon}
+            alt="GitHub"
+            className="icon"
+            onClick={() => window.location.href='https://github.com/'}
+          />
         </div>
       </div>
     </section>
